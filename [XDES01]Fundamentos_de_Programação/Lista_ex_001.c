4807ce2fe315be3b967d4a6378a16d338d7b917a
Kendi <54328665+vkendis@users.noreply.github.com>
2023-04-14 22:26:54 -0300

    .

---
+++ [XDES01]Fundamentos_de_Programação/Lista_ex_001.c
@@ -0,0 +1,9 @@
+//1) Fazer um programa que leia um número inteiro e o imprima na tela.
+#include <stdio.h>
+
+int main(void) {
+int num;
+  printf("Digite um numero: \n" );
+  scanf("%d", &num);
+  printf("O numero digitado foi: %d", num);
+}
